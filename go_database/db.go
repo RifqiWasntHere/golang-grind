721d348712d10f61093ca91f7b7fc894d312dafd
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-09-17 17:30:40 +0700

    Added GetDatabase Func

---
+++ go_database/db.go
@@ -0,0 +1,37 @@
+package go_database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func GetDatabase() *sql.DB {
+	// load godotenv
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Failed to load .env")
+	}
+
+	// Retrieve environment variables
+	dbCreds := os.Getenv("DB_CREDS")
+	dbName := os.Getenv("DB_NAME")
+
+	// open db connection
+	db, err := sql.Open("mysql", fmt.Sprintf("%s/%s?parseTime=true", dbCreds, dbName))
+	if err != nil {
+		log.Fatalln("Failed to connect to database")
+	}
+
+	db.SetMaxIdleConns(5)                   //Max. amount of idle dbconn
+	db.SetMaxOpenConns(10)                  //Max. amount of dbconn at a same time
+	db.SetConnMaxIdleTime(5 * time.Minute)  //Max. lifetime a dbconn can idle
+	db.SetConnMaxLifetime(60 * time.Minute) //Max. lifetime of a dbconn
+
+	return db
+}
