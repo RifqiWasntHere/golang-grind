92f2f2b67fdd4b3bb7f4e707efaa7f4e96248c9a
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-09-19 17:16:22 +0700

    Added User Repository

---
+++ go_database/repository/user_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"context"
+	"go_database/model"
+)
+
+type UserRepository interface {
+	Insert(ctx context.Context, user model.User) (model.User, error)
+	FindById(ctx context.Context, user model.User) (model.User, error)
+}
