4542072e98efaa2225e00ae659a3bbea108aa9c8
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-09-24 17:11:02 +0700

    Added Error Handling Middleware Practice

--- golang_web/middleware_test.go
+++ golang_web/middleware_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+// Basic Middleware
 type LogMiddleware struct {
 	Handler http.Handler
 }
@@ -16,6 +17,24 @@ func (middleware *LogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Reques
 	middleware.Handler.ServeHTTP(w, r)
 }
 
+// Error Handler Middleware
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorhandler *ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("An Server Error Has Occured", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "Internal Server Error")
+		}
+	}()
+
+	errorhandler.Handler.ServeHTTP(w, r)
+}
+
 func TestMiddleware(t *testing.T) {
 	mux := http.NewServeMux()
 
@@ -23,14 +42,21 @@ func TestMiddleware(t *testing.T) {
 		fmt.Println("User Accessed /home")
 		fmt.Fprint(w, "Welcome Home")
 	})
+	mux.HandleFunc("/restricted", func(w http.ResponseWriter, r *http.Request) {
+		panic("BYE BYE")
+	})
 
 	LogMiddleware := LogMiddleware{
 		Handler: mux,
 	}
 
+	ErrorHandler := ErrorHandler{
+		Handler: &LogMiddleware,
+	}
+
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: &LogMiddleware,
+		Handler: &ErrorHandler,
 	}
 
 	err := server.ListenAndServe()
