3e0ac615b9dcf0a9742396e8f68fc6d1b0be4d65
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-09-25 17:07:39 +0700

    Added Panic Handler Practice

--- go_httprouter/router_test.go
+++ go_httprouter/router_test.go
@@ -11,15 +11,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// This Params Pattern is Named : Named Parameter
 func TestHttpRouter(t *testing.T) {
 
 	router := httprouter.New()
-	router.GET("/product/:id", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-		productId := params.ByName("id")
-		fmt.Fprintf(w, "Product "+productId)
+	router.GET("/concert/:concertId/ticket/:ticketId", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		concertId, ticketId := params.ByName("concertId"), params.ByName("concertId")
+		fmt.Fprintf(w, "Concert : %s, Ticket ID : %s", concertId, ticketId)
 	})
 
-	req := httptest.NewRequest("GET", "http://localhost:8080/product/1", nil)
+	req := httptest.NewRequest("GET", "http://localhost:8080/concert/kickyourass/ticket/AsdDSjkSc123e", nil)
 	rec := httptest.NewRecorder()
 
 	router.ServeHTTP(rec, req)
@@ -28,5 +29,27 @@ func TestHttpRouter(t *testing.T) {
 	body, _ := io.ReadAll(res.Body)
 
 	// Testing
-	assert.Equal(t, "Product 1", string(body))
+	assert.Equal(t, "Concert : kickyourass, Ticket ID : kickyourass", string(body))
+}
+
+// Panic Handler
+func TestPanicHandler(t *testing.T) {
+
+	router := httprouter.New()
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
+		fmt.Fprint(w, "Internal Server Error : ", i)
+	}
+	router.GET("/panic", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		panic("oopsies")
+	})
+
+	req := httptest.NewRequest("GET", "http://localhost:8080/panic", nil)
+	rec := httptest.NewRecorder()
+
+	router.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	body, _ := io.ReadAll(res.Body)
+
+	assert.Equal(t, "Internal Server Error : oopsies", string(body))
 }
