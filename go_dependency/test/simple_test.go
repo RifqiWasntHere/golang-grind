6c570fd6317369b59c6aec408227650bc81a07e9
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-10-15 17:23:33 +0700

    Added dependency injection test

---
+++ go_dependency/test/simple_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"go_restful_api/simple"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializeService(true)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleService, err := simple.InitializeService(false)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, simpleService)
+}
