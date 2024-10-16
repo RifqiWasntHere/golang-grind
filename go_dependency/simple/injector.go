7feb7897977d875af20f23a837df0cf2305b266f
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-10-16 16:28:48 +0700

    Added new dependency injection practices

--- go_dependency/simple/injector.go
+++ go_dependency/simple/injector.go
@@ -12,6 +12,7 @@ func InitializeService(isError bool) (*SimpleService, error) {
 	return nil, nil
 }
 
+// Multiple binding
 func InitializeDatabase() *DatabaseRepository {
 	wire.Build(
 		NewDatabasePostgreSQL,
@@ -21,6 +22,7 @@ func InitializeDatabase() *DatabaseRepository {
 	return nil
 }
 
+// Provider set
 var fooSet = wire.NewSet(NewFooRepository, NewFooService)
 var barSet = wire.NewSet(NewBarRepository, NewBarService)
 var fooBarSet = wire.NewSet(NewFooBarService)
@@ -28,5 +30,36 @@ var fooBarSet = wire.NewSet(NewFooBarService)
 func InitializeFooBarService() *FooBarService {
 	wire.Build(fooSet, barSet, fooBarSet) //fooBarSet is best kept hardcoded in the build for efficiency
 
+	return nil
+}
+
+// Binding Interface
+// SayHello sets
+var helloSet = wire.NewSet(
+	NewSayHelloImpl,
+	wire.Bind(new(SayHello), new(*SayHelloImpl)), //new() is used to return pointer of it's parameter (equals to pointer type conversion [(SayHello)(nil)])
+)
+
+func InitializeSayHelloService() *SayHelloService {
+	wire.Build(helloSet, NewSayHelloService)
+	return nil
+}
+
+// Struct Provider
+var fooSet2 = wire.NewSet(NewFooRepository, NewFooService)
+var barSet2 = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializeFooBarStruct() *FooBarService {
+	wire.Build(fooSet2, barSet2, wire.Struct(new(FooBarService), "FooService", "BarService")) // Wilcard is available to assign all struct's field "*"
+	return nil
+}
+
+// Binding Values
+
+var fooValue = &FooService{}
+var barValue = &BarService{}
+
+func InitializeFoobarStructWithValues() *FooBarService {
+	wire.Build(wire.Value(fooValue), wire.Value(barValue), wire.Struct(new(FooBarService), "*"))
 	return nil
 }
