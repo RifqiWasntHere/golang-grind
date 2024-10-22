f6fb010d265f8f9c80bf595df1b9d4a93273cfed
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-10-22 14:58:15 +0700

    Added formatter practice

--- golang_logging/logger_test.go
+++ golang_logging/logger_test.go
@@ -29,3 +29,25 @@ func TestOutput(t *testing.T) {
 	logger.Warn("Watchamachalit")
 	logger.Error("Watchamachalit")
 }
+
+func TestFormatter(t *testing.T) {
+	logger := logrus.New()
+
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
+	// logger.SetFormatter(&logrus.TextFormatter{
+	// 	DisableTimestamp: true,
+	// 	DisableQuote:     true,
+	// })
+	logger.SetFormatter(&logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{ //FieldMap is used to rename the field names witin JSON formatter
+			logrus.FieldKeyTime: "@timestamp",
+			// logrus.FieldKeyMsg:  "@message",
+			logrus.FieldKeyLevel: "nil",
+		},
+	})
+
+	logger.Info("Watchamachalit")
+	logger.Warn("Watchamachalit")
+	logger.Error("Watchamachalit")
+}
